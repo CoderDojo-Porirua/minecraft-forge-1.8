6b1422dbc84a115171ab41303ba6c3a5e7b5e39f
Leigh Hunt <leigh@venari.co.nz>
2016-05-21 00:42:33 +1200

    Adding init and postInit

--- src/main/java/nz/cdr/minecrafttutorial/TutorialMod.java
+++ src/main/java/nz/cdr/minecrafttutorial/TutorialMod.java
@@ -1,10 +1,20 @@
 package nz.cdr.minecrafttutorial;
 
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class TutorialMod {
 
 	public void preInit(FMLPreInitializationEvent event)
+	{
+	
+	}
+	public void init(FMLInitializationEvent event)
+	{
+	
+	}
+	public void postInit(FMLPostInitializationEvent event)
 	{
 	
 	}
