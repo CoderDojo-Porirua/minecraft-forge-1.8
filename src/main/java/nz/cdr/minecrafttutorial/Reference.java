35a2467a3ce18fbccf14105e1b83f6b028093e8e
Leigh Hunt <leigh@venari.co.nz>
2016-05-21 01:42:41 +1200

    Mod name

--- src/main/java/nz/cdr/minecrafttutorial/Reference.java
+++ src/main/java/nz/cdr/minecrafttutorial/Reference.java
@@ -2,6 +2,6 @@ package nz.cdr.minecrafttutorial;
 
 public class Reference {
 	public static final String MOD_ID = "tm";
-	public static final String MOD_NAME = "Tutorial Mod";
+	public static final String MOD_NAME = "CoderDojo Tutorial Mod";
 	public static final String VERSION = "1.0";
 }
