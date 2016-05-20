02f5f6342229f3d38f7448eb934a0dcc7024d8d9
Leigh Hunt <leigh@venari.co.nz>
2016-05-21 00:37:45 +1200

    Adding preInit

--- src/main/java/nz/cdr/minecrafttutorial/TutorialMod.java
+++ src/main/java/nz/cdr/minecrafttutorial/TutorialMod.java
@@ -1,5 +1,11 @@
 package nz.cdr.minecrafttutorial;
 
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
 public class TutorialMod {
 
+	public void preInit(FMLPreInitializationEvent event)
+	{
+	
+	}
 }
