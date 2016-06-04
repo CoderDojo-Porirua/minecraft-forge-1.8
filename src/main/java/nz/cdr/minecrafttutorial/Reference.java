8f2abfbca9c9eac07c12c984697d5a5149ee6413
Leigh Hunt <leigh@venari.co.nz>
2016-06-04 17:43:53 +1200

    Adding face texture

--- src/main/java/nz/cdr/minecrafttutorial/Reference.java
+++ src/main/java/nz/cdr/minecrafttutorial/Reference.java
@@ -2,7 +2,7 @@ package nz.cdr.minecrafttutorial;
 
 public class Reference {
 	public static final String MOD_ID = "tm";
-	public static final String MOD_NAME = "CoderDojo Tutorial Mod";
+	public static final String MOD_NAME = "CoderDojo is cool!";
 	public static final String VERSION = "1.0";
 	public static final String CLIENT_PROXY_CLASS = "nz.cdr.minecrafttutorial.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "nz.cdr.minecrafttutorial.proxy.CommonProxy";
